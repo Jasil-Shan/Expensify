b63ee3792c52958a120223243aadaad8d8675a9e
Jasil Shan <jasilshan786@gmail.com>
2024-01-06 18:41:23 +0530

    bug fix

--- Server/routes/userRouter.js
+++ Server/routes/userRouter.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import { login, signup } from '../controller/authController.js'
+import { verifyUser } from '../middleware/verifyUser.js'
+import { addBudget, addExpense, deleteExpense, getExpenses } from '../controller/expenseController.js'
 
 
 const router = express.Router()
@@ -8,6 +10,13 @@ const router = express.Router()
 
 router.post('/signup',signup)
 router.post('/login',login)
+
+router.use(verifyUser)
+router.post('/addExpense',addExpense)
+router.post('/deleteExpense/:id',deleteExpense)
+router.post('/addBudget',addBudget)
+
+router.get('/getExpense',getExpenses)
 
 
 export default router
